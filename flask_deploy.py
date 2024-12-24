d97fbfbab68d62dd167832dd2519a72504d350d5
Rafael Deflon <106821774+rafaeldeflon@users.noreply.github.com>
2024-12-24 14:48:08 -0300

    Update flask_deploy.py

--- flask_deploy.py
+++ flask_deploy.py
@@ -1,3 +1,5 @@
+# Set up of the Flask app
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 import joblib
@@ -5,7 +7,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Enable CORS for all routes
+# Enable CORS for all routes (I had some issues with this setting)
 CORS(app)
 
 # Load the saved Random Forest model
@@ -15,7 +17,7 @@ except Exception as e:
     rf_model = None
     print(f"Model loading failed: {e}")
 
-# Define the list of features (ensure this matches the training data)
+# Define the list of features (the variables that we checked on the exploratory analysis)
 significant_features = ['fixed acidity', 'volatile acidity', 'citric acid',
                         'chlorides', 'free sulfur dioxide', 'total sulfur dioxide',
                         'density', 'pH', 'sulphates', 'alcohol']
@@ -25,31 +27,32 @@ def predict():
     if rf_model is None:
         return jsonify({'error': 'Model not loaded properly'}), 500
     
-    # Get the JSON data from the request
+    # Get the JSON data
     data = request.get_json()
+
+# Validate the input data
     
-    # Validate the input data
     if not isinstance(data, list):
         return jsonify({'error': 'Invalid input format. Expected a list of dictionaries.'}), 400
     
-    # Convert the JSON data to a DataFrame
+    # 1. Convert the JSON to a DataFrame
     df = pd.DataFrame(data)
 
-    # Ensure the DataFrame has the required columns
+    # 2. Ensure the DataFrame has the required columns
     missing_features = [feature for feature in significant_features if feature not in df.columns]
     if missing_features:
         return jsonify({'error': f'Missing features: {", ".join(missing_features)}'}), 400
 
-    # Predict using the Random Forest model
+# Predict using the Random Forest model
     try:
         predictions = rf_model.predict(df[significant_features])
     except Exception as e:
         return jsonify({'error': f'Prediction failed: {e}'}), 500
 
-    # Map predictions to labels
+# Map predictions to labels
     predicted_labels = ['good' if pred == 1 else 'bad' for pred in predictions]
 
-    # Return predictions as JSON
+# Return predictions as JSON
     return jsonify({'predictions': predicted_labels})
 
 if __name__ == '__main__':
